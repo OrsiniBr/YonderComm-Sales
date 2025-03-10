d9c6b74ada105ab84cb5ecaf534d9e93183299c2
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2025-03-10 19:01:10 +0100

    routes and controllers created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+//routes
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config(); 
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(1000, ()=>{
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
     console.log("server is running on http://localhost:" + PORT)
-});
+}); 
