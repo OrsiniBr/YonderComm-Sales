f004d32423ac12d524ec978020608f4c06aeca01
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2025-03-17 19:26:02 +0100

    implement coupon feature

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from './routes/coupon.route.js'
 
 dotenv.config(); 
 
@@ -16,6 +17,7 @@ app.use(cookieParser());    //allows you parse cookies
 app.use("/api/auth", authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes)
+app.use('/api/coupons', couponRoutes)
 
 app.listen(PORT, () => {
     console.log("server is running on http://localhost:" + PORT);
