35563caa6be0ceded48c968e91403bf9c6a443fa
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2025-03-17 20:57:27 +0100

    impl payment feature

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from './routes/coupon.route.js'
+import paymentRoutes from './routes/payment.route.js'
+
 
 dotenv.config(); 
 
@@ -16,8 +18,9 @@ app.use(express.json()); //allows you parse body of requests
 app.use(cookieParser());    //allows you parse cookies
 app.use("/api/auth", authRoutes);
 app.use('/api/products', productRoutes);
-app.use('/api/cart', cartRoutes)
-app.use('/api/coupons', couponRoutes)
+app.use('/api/cart', cartRoutes);
+app.use('/api/coupons', couponRoutes);
+app.use('/api/payments', paymentRoutes)
 
 app.listen(PORT, () => {
     console.log("server is running on http://localhost:" + PORT);
