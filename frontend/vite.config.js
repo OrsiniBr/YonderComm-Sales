1eb66ec84dcc7611deae1c4c9f17bff81256bc83
orsini <udochukwugodwin13@gmail.com>
2025-04-29 16:23:21 +0100

    (6)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     proxy: {
+//       '/api': { 
+//         target: 'http://localhost:5000' ,
+//       }
+//     }
+//   }
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': { 
-        target: 'http://localhost:5000' ,
-      }
-    }
-  }
-})
-
-
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
+});
